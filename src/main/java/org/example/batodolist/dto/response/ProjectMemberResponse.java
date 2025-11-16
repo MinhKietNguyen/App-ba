ca9d1d612c7c0866738dadeea90380cbaa5eb5b3
kietnm.b22vt270@stu.ptit.edu.vn <ngocltm.tts@ncb-bank.vn>
2025-11-17 00:42:46 +0700

    Mấy Project, Project Member, Reminder t đang fix, test mấy cái còn lại

--- src/main/java/org/example/batodolist/dto/response/ProjectMemberResponse.java
+++ src/main/java/org/example/batodolist/dto/response/ProjectMemberResponse.java
@@ -1,11 +1,13 @@
 package org.example.batodolist.dto.response;
 
+import lombok.Data;
 import org.example.batodolist.common.ProjectMemberRole;
 import org.example.batodolist.model.Project;
 import org.example.batodolist.model.User;
 
 import java.time.LocalDateTime;
 
+@Data
 public class ProjectMemberResponse {
     private Long id;
     private Project project;
--- src/main/java/org/example/batodolist/dto/response/ProjectResponse.java
+++ src/main/java/org/example/batodolist/dto/response/ProjectResponse.java
@@ -1,10 +1,12 @@
 package org.example.batodolist.dto.response;
 
+import lombok.Data;
 import org.example.batodolist.model.User;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Data
 public class ProjectResponse {
     private Long id;
     private String name;
