91662792cd1a68357098fd6d63c8626f130a425c
kietnm.b22vt270@stu.ptit.edu.vn <ngocltm.tts@ncb-bank.vn>
2025-11-17 14:00:50 +0700

    Fix conflict

--- src/main/java/org/example/batodolist/service/implement/ProjectImplementService.java
+++ src/main/java/org/example/batodolist/service/implement/ProjectImplementService.java
@@ -7,11 +7,8 @@ import org.example.batodolist.dto.request.ProjectRequest;
 import org.example.batodolist.dto.request.ProjectUpdateRequest;
 import org.example.batodolist.dto.response.ProjectResponse;
 import org.example.batodolist.model.Project;
-import org.example.batodolist.model.ProjectMember;
 import org.example.batodolist.model.User;
-import org.example.batodolist.repo.ProjectMemberRepository;
 import org.example.batodolist.repo.ProjectRepository;
-import org.example.batodolist.repo.UserRepository;
 import org.example.batodolist.service.ProjectService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +22,10 @@ import java.time.LocalDateTime;
 @Service
 public class ProjectImplementService implements ProjectService {
     private final ProjectRepository projectRepository;
-    private final UserRepository userRepository;
-    private final ProjectMemberRepository projectMemberRepository;
 
     @Autowired
-    public ProjectImplementService(ProjectRepository projectRepository, UserRepository userRepository, ProjectMemberRepository projectMemberRepository) {
+    public ProjectImplementService(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
-        this.userRepository = userRepository;
-        this.projectMemberRepository = projectMemberRepository;
     }
 
     @Override
@@ -40,15 +33,6 @@ public class ProjectImplementService implements ProjectService {
         Project project = projectRepository.findById(id).orElseThrow(() -> new BadRequestException(ErrorCode.NOT_FOUND));
         ProjectResponse projectResponse = new ProjectResponse();
         BeanUtils.copyProperties(project, projectResponse);
-
-        User user = project.getManager();
-        UserDTO userDTO = new UserDTO();
-        BeanUtils.copyProperties(user, userDTO);
-
-        ProjectMember projectMember = projectMemberRepository.findProjectMemberByProjectId(id);
-        userDTO.setRoleInProject(projectMember.getRoleInProject());
-
-        projectResponse.setManager(userDTO);
         return projectResponse;
     }
 
@@ -57,18 +41,10 @@ public class ProjectImplementService implements ProjectService {
         ProjectResponse projectResponse = new ProjectResponse();
         Project project = new Project();
         BeanUtils.copyProperties(projectRequest, project);
-
-        User user = projectRequest.getManager() == null ?
-                null : userRepository.findUserByUsername(projectRequest.getManager()).orElseThrow(()
-                -> new BadRequestException(ErrorCode.NOT_FOUND));
-        project.setManager(user);
         project.setCreatedAt(LocalDateTime.now());
         project.setUpdatedAt(null);
-
         projectRepository.save(project);
         BeanUtils.copyProperties(project, projectResponse);
-
-        projectResponse.setManager(user == null ? null : convertToUserDTO(user));
         return projectResponse;
     }
 
@@ -77,17 +53,9 @@ public class ProjectImplementService implements ProjectService {
         ProjectResponse projectResponse = new ProjectResponse();
         Project project = projectRepository.findById(id).orElseThrow(() -> new BadRequestException(ErrorCode.NOT_FOUND));
         BeanUtils.copyProperties(projectRequest, project);
-
-        User user = projectRequest.getManager() == null ?
-                null : userRepository.findUserByUsername(projectRequest.getManager()).orElseThrow(()
-                -> new BadRequestException(ErrorCode.NOT_FOUND));
-        project.setManager(user);
         project.setUpdatedAt(LocalDateTime.now());
-
         projectRepository.save(project);
         BeanUtils.copyProperties(project, projectResponse);
-
-        projectResponse.setManager(user == null ? null : convertToUserDTO(user));
         return projectResponse;
     }
 
@@ -103,21 +71,7 @@ public class ProjectImplementService implements ProjectService {
                 x -> {
                     ProjectResponse projectResponse = new ProjectResponse();
                     BeanUtils.copyProperties(x, projectResponse);
-
-                    User user = x.getManager();
-                    UserDTO userDTO = new UserDTO();
-                    BeanUtils.copyProperties(user, userDTO);
-
-                    ProjectMember projectMember = projectMemberRepository.findProjectMemberByProjectId(x.getId());
-                    userDTO.setRoleInProject(projectMember.getRoleInProject());
-                    projectResponse.setManager(userDTO);
-
                     return projectResponse;
                 });
-    }
-    private UserDTO convertToUserDTO(User user) {
-        UserDTO userDTO = new UserDTO();
-        BeanUtils.copyProperties(user, userDTO);
-        return userDTO;
     }
 }
